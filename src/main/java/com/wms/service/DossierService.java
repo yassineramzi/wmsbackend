f9afcce62df42ed0885d87016ad64d5c74ef819d
yassineramzi <yassine.ramzi2010@gmail.com>
2022-09-06 16:57:55 +0100

    Update DossierService.java

--- src/main/java/com/wms/service/DossierService.java
+++ src/main/java/com/wms/service/DossierService.java
@@ -7,4 +7,5 @@ import com.wms.service.dto.DossierDTO;
 public interface DossierService {
     DossierDTO create(final DossierDTO dossierDTO);
     List<DossierDTO> findAll();
+    void delete(final Long id);
 }
