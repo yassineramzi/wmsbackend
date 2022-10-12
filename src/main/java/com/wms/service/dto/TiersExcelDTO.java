983f2ab7f864889ab20635676aa9c474fc645f37
RAMZI Yassine <Yassine.RAMZI@cegedim.com>
2022-10-12 16:24:38 +0100

    Edit

--- src/main/java/com/wms/service/dto/TiersExcelDTO.java
+++ src/main/java/com/wms/service/dto/TiersExcelDTO.java
@@ -20,47 +20,48 @@ public class TiersExcelDTO implements Serializable{
     @NotEmpty
     @Pattern(regexp = "^[0-9]{10}$")
     private String identifiantFiscal;
+    /* 
+        @NotEmpty
+        private String designation;
 
-    @NotEmpty
-    private String designation;
+        @NotEmpty
+        private String activite;
 
-    @NotEmpty
-    private String activite;
+        @NotEmpty
+        private String adresse;
 
-    @NotEmpty
-    private String adresse;
+        @NotEmpty
+        @Pattern(regexp = "^[0-9]{24}$")
+        private String rib;
 
-    @NotEmpty
-    @Pattern(regexp = "^[0-9]{24}$")
-    private String rib;
+        @Email
+        private String email;
 
-    @Email
-    private String email;
+        @NotEmpty
+        @Pattern(regexp = "^[0-9]{15}$")
+        private String ice;
 
-    @NotEmpty
-    @Pattern(regexp = "^[0-9]{15}$")
-    private String ice;
+        @NotEmpty
+        private String registreCommerce;
 
-    @NotEmpty
-    private String registreCommerce;
+        @NotEmpty
+        @Pattern(regexp = "^[0-9]{10}$")
+        private String telephone;
 
-    @NotEmpty
-    @Pattern(regexp = "^[0-9]{10}$")
-    private String telephone;
+        private String ville;
 
-    private String ville;
+        @NotEmpty
+        private String banque;
 
-    @NotEmpty
-    private String banque;
+        @NotEmpty
+        private String echeance;
 
-    @NotEmpty
-    private String echeance;
+        @NotEmpty
+        private String typeEcheance;
 
-    @NotEmpty
-    private String typeEcheance;
-
-    @NotEmpty
-    private String devise;
+        @NotEmpty
+        private String devise;
+    */
 
     public static boolean isValid(
         @Validated final TiersExcelDTO tiersExcelDTO,
